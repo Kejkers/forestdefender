343d60d872ebd11eeaee50d9a04770712db40298
khmhmm <khmhmm@ya.ru>
2022-04-02 17:02:47 +0400

    feat: Dagger combat

--- Assets/Scripts/Enemies/BaseWalker.cs
+++ Assets/Scripts/Enemies/BaseWalker.cs
@@ -2,27 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseWalker : MonoBehaviour
+public class BaseWalker : BaseEnemy
 {
-    public float hp = 1f;
-
-    protected void OnTriggerEnter2D(Collider2D col) {
-        Debug.Log("Shot");
-        var proj = col.GetComponent<Projectile>();
-        if (proj != null) {
-            proj.ConfirmShot(this.transform);
-            GetHurt(proj.GetDamage());
-        }
-    }
-
-    public void GetHurt(float damage) {
-        hp -= damage;
-        if (hp <= 0f) {
-            Death();
-        }
-    }
-
-    protected void Death() {
-
+    protected new void OnTriggerEnter2D(Collider2D col) {
+        base.OnTriggerEnter2D(col);
     }
 }
